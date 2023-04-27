869e103090298852e1094ea6db2553033ba29c1d
HerrPablito <107303243+HerrPablito@users.noreply.github.com>
2023-04-27 21:39:51 +0200

    update

--- controller/notes.controller.js
+++ controller/notes.controller.js
@@ -1,6 +1,6 @@
 const { saveNote, seeMyNotes, deleteNote, updateNote, findNote } = require("../model/notes.model");
 const { findUserById, findUserByName } = require("../model/user.model");
-
+const { notesDb } = require('../model/notes.model')
 
 async function saveNoteCtrl(request, response) {
     try {
@@ -25,11 +25,11 @@ async function saveNoteCtrl(request, response) {
 
 async function showNotesCtrl(request, response) {
     try {
-        
-      } catch (error) {
+
+    } catch (error) {
         console.error(error);
-        response.status(500).send('Internal Server Error'); 
-      }
+        response.status(500).send('Internal Server Error');
+    }
     const { userId } = request.params;
     const myNotes = await seeMyNotes(userId)
 
@@ -45,61 +45,73 @@ async function showNotesCtrl(request, response) {
 
 async function deletenNoteByIdCtrl(request, response) {
     try {
-            const { noteId } = request.params;
-    const deletedNote = await deleteNote(noteId)
+        const { noteId } = request.params;
+        const deletedNote = await deleteNote(noteId)
 
-    if (deletedNote) {
-        response.json({
-            success: true,
-            message: "note deleted"
-        })
-    } else {
-        response.json({ success: false })
-    }
+        if (deletedNote) {
+            response.json({
+                success: true,
+                message: "note deleted"
+            })
+        } else {
+            response.json({ success: false })
+        }
     } catch (error) {
-      console.error(error);
-      response.status(500).send('Internal Server Error'); 
+        console.error(error);
+        response.status(500).send('Internal Server Error');
     }
 
 }
 
 async function updateNoteCtrl(request, response) {
     try {
-      const { noteId, title } = request.body;
-    const updatedNote = await updateNote(noteId, title)
+        const { noteId, title } = request.body;
+        const noteIdExist = await notesDb.findOne({ noteId: noteId })
+        if (!noteIdExist) {
 
-    if (updatedNote) {
-        response.json({
-            success: true,
-            message: "note updated"
-        })
-    } else {
-        response.json({ success: false })
-    }      
+            console.log("den finns inte");
+            response.json({
+                success: false,
+                message: 'Note ID does not exist'
+            })
+        }else{
+        const updatedNote = await updateNote(noteId, title)
+
+        if (updatedNote) {
+            response.json({
+                success: true,
+                message: "note updated"
+            })
+        } else {
+            response.json({ success: false })
+        }            
+        }
+
+
     } catch (error) {
-      console.error(error);
-      response.status(500).send('Internal Server Error'); 
+        console.error(error);
+        response.status(500).send('Internal Server Error');
     }
 
 }
 
 async function searchNoteByTitleCtrl(request, response) {
     try {
-    const { title } = request.params
-    const showSeachedNote = await findNote(title)
+        const { title } = request.params
+        const showSeachedNote = await findNote(title)
 
-    if (showSeachedNote) {
-        response.json({
-            success: true,
-            note: showSeachedNote
+        if (showSeachedNote) {
+            response.json({
+                success: true,
+                note: showSeachedNote
 
-        })
-    } else {
-        response.json({ success: false })
-    }        
+            })
+        } else {
+            response.json({ success: false })
+        }
     } catch (error) {
-      console.error(error);
-      response.status(500).send('Internal Server Error'); 
+        console.error(error);
+        response.status(500).send('Internal Server Error');
     }
 
 }
